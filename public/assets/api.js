170abd12dffbbd4e394b4763efdb67b70c510bc6
Nathan Polomark <polomarkn@hotmail.com>
2020-10-31 16:48:16 -0700

    Added second API call, commented out cuisine type for now

--- public/assets/api.js
+++ public/assets/api.js
@@ -1,63 +1,108 @@
-let cuisineType = $('#cuisineType').val()
+
 let intolerance = $('#intolerance').val()
 let foodItem = $('#foodItem').val()
 
 
 
-$('#searchBtn').on("click", function(event) {
+// $('#searchBtn').on("click", function(event) {
 
+//     console.log(foodItem)
+
+//     console.log(intolerance)
+//     searchRecipe()
+// })
+
+// function searchRecipe() {
+
+//     intolerance = $('#intolerance').val()
+//     foodItem = $('#foodItem').val()
+
+//     const intolerances = `&intolerances=${intolerance}`
+//     const food = `&query=${foodItem}`
+//     let settings = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?offset=0&number=10" + `${intolerances}`+ `${food}`,
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+//             "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+//         }
+//     }
+    
+//     $.ajax(settings).done(function (response) {
+//         console.log(response)
+//         const results = response.results
+//         for (let i = 0; i < results.length; i++) {
+//             $('#apiCall').append(`
+//             <div class="card" style="width: 18rem;">
+//             <h5 class="card-title">${results[i].title}</h5>
+//             <img src="${response.baseUri + results[i].image}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//             <p class="card-text">Estimated time: ${results[i].readyInMinutes} min</p>
+//             <p class="card-text"> Servings: ${results[i].servings}
+//             <a href="${results[i].sourceUrl}" class="btn btn-primary">Go somewhere</a>
+//             </div>
+//             </div>
+//             `)
+//         }
+//     });
+// }
+
+$('#searchBtn').on("click", function(event) {
     console.log(foodItem)
-    console.log(cuisineType)
-    console.log(intolerance)
-    searchRecipe()
+
+    searchRecipeIngredients()
 })
 
-function searchRecipe() {
-    cuisineType = $('#cuisineType').val()
-    intolerance = $('#intolerance').val()
+function searchRecipeIngredients() {
     foodItem = $('#foodItem').val()
-    const cuisine = `&cuisine=${cuisineType}`
-    const intolerances = `&intolerances=${intolerance}`
-    const food = `&query=${foodItem}`
-    let settings = {
+    var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?offset=0&number=10" + `${cuisine}` + `${intolerances}`+ `${food}`,
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=10&ranking=1&ignorePantry=false&ingredients=" + `${foodItem}`,
         "method": "GET",
         "headers": {
-            "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
             "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
         }
     }
     
     $.ajax(settings).done(function (response) {
-        console.log(response)
-        const results = response.results
-        for (let i = 0; i < results.length; i++) {
-            $('#apiCall').append(`
-            <div class="card" style="width: 18rem;">
-            <h5 class="card-title">${results[i].title}</h5>
-            <img src="${response.baseUri + results[i].image}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <p class="card-text">Estimated time: ${results[i].readyInMinutes} min</p>
-            <p class="card-text"> Servings: ${results[i].servings}
-            <a href="${results[i].sourceUrl}" class="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>
-            `)
+        console.log(response);
+
+        for (let i = 0; i < response.length; i++) {
+            let recipeId = response[i].id
+
+            function getRecipeInfo() {
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + `${recipeId}` + "/information",
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+                    }
+                }
+                
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                    $('#apiCall').append(`
+                    <div class="card" style="width: 18rem;">
+                        <h5 class="card-title">${response.title}</h5>
+                        <img src="${response.image}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <p class="card-text">Estimated time: ${response.readyInMinutes} min</p>
+                            <p class="card-text"> Servings: ${response.servings}
+                            <a href="${response.sourceUrl}" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    `)
+                });
+            }
+        getRecipeInfo()
         }
+        
     });
 }
-// $('#searchBtn').on("click", function(event) {
-//     searchRecipe()
-//     event.preventDefault()
-// })
-
-
-
-
-// $('').text(results[i].title)
-// $('').text(response.baseUri + results[i].image)
-// $('').text(results[i].readyInMinutes)
-// $('').text(results[i].servings)
-// $('').text(results[i].sourceUrl)
