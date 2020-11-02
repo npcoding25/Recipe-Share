060158ea79dfc43650e346bf6bed1dd2a0ab671f
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-02 15:10:44 -0500

    eslint

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,4 +1,4 @@
-let loginUser = ""
+let loginUser = ''
 
 $('#signUp').on('click', NewUser())
 $('#login').on('click', Login())
@@ -8,27 +8,27 @@ $('#homeTitle').on('click', UserRecipes())
 $('#deleteBtn').on('click', DelRecipes())
 
 function NewUser() { // Create new user
-    
+
     let username = $('#loginUsername')
     let password = $('#loginPassword')
     data = {
-      username: `${username}`,
-      password: `${password}`
+        username: `${username}`,
+        password: `${password}`
     };
     $.post('/api/user', data, function (result) { // Optionally display message
-      loginUser = result.insertId
+        loginUser = result.insertId
     });
-  }
+}
 
-  function Login() {
+function Login() {
     // Login user
     $.post('/api/userAuth', data, function (result) { // Optionally display message
-      loginUser = result.insertId
+        loginUser = result.insertId
     });
-  }
+}
 
-  
-  function NewRecipe() {
+
+function NewRecipe() {
 
     const newRecipeTitle = $('#exampleFormControlInput2')
     const newRecipeDescription = $('#exampleFormControlTextarea1')
@@ -36,44 +36,44 @@ function NewUser() { // Create new user
     const newRecipeImage = $('.imageURL')
     // Post another recipe.
     data = {
-      title: `${newRecipeTitle}`,
-      description: `${newRecipeDescription}`,
-      sourceUrl: `${newRecipeUrl}`,
-      imageUrl: `${newRecipeImage}`
+        title: `${newRecipeTitle}`,
+        description: `${newRecipeDescription}`,
+        sourceUrl: `${newRecipeUrl}`,
+        imageUrl: `${newRecipeImage}`
     };
     userId = loginUser;
     $.post(`/api/recipe/${userId}`, data, function (result) { // Optionally display message
-      recipeId = result.insertId; // save for delete
+        recipeId = result.insertId; // save for delete
     });
-  }
+}
 
-  function AllRecipes() {
-   
+function AllRecipes() {
+
     // Get all recipes
     $.get('/api/recipe', function (result) {
-      result.forEach(recipe => {
-        let curhtml = $('#apiCall').html();
-        $('#apiCall').prepend(curhtml += `           
-        <div class="card" style="width: 18rem;">
-            <h5 class="card-title">${recipe.title}</h5>
-            <img src="${recipe.imageUrl}" class="card-img-top" alt="...">
-            <div class="card-body">
+        result.forEach(recipe => {
+            let curhtml = $('#apiCall').html();
+            $('#apiCall').prepend(curhtml += `           
+            <div class="card" style="width: 18rem;">
+              <h5 class="card-title">${recipe.title}</h5>
+              <img src="${recipe.imageUrl}" class="card-img-top" alt="...">
+              <div class="card-body">
                 <p class="card-text">${recipe.description}</p>
                 <a href="${recipe.sourceUrl}" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
-        </div>
-        `);
-      });
+            `);
+        });
     });
-  }
+}
 
-  function UserRecipes() {
+function UserRecipes() {
     // Get recipes by user
     userId = loginUser;
     $.get(`/api/recipe/${userId}`, function (result) {
-      result.forEach(recipe => {
-        let curhtml = $('#apiCall').html();
-        $('#apiCall').prepend(curhtml += `
+        result.forEach(recipe => {
+            let curhtml = $('#apiCall').html();
+            $('#apiCall').prepend(curhtml += `
         <div class="card" style="width: 18rem;">
             <h5 class="card-title">${recipe.title}</h5>
             <img src="${recipe.imageUrl}" class="card-img-top" alt="...">
@@ -83,16 +83,16 @@ function NewUser() { // Create new user
             </div>
         </div>
         `);
-      });
+        });
     });
-  }
+}
 
-  function DelRecipe() {
+function DelRecipe() {
     // Optional: delete recipe
     $.ajax({
-        url: `/api/recipe/${recipeId}`, 
+        url: `/api/recipe/${recipeId}`,
         method: 'DELETE'
     }).then(function (result) { // Optionally display message
-      console.log(result)
+        console.log(result)
     });
-  }
+}
