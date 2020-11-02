6163132f1f4be28e5ad2031a227dce9bfe6eab02
Nathan Polomark <polomarkn@hotmail.com>
2020-11-02 07:26:43 -0800

    Changed class to and id

--- public/assets/api.js
+++ public/assets/api.js
@@ -1,52 +1,52 @@
-let cuisineType = $('.cuisineType').val()
+let cuisineType = $('#cuisineType').val()
 let intolerance = $('#intolerance').val()
 let foodItem = $('#foodItem').val()
 let ingredientItem = $('#ingredientItems')
 
 
-$('#searchBtn').on("click", function(event) {
+$('#searchBtn').on("click", function (event) {
     searchRecipe()
 })
 
 
 function searchRecipe() {
-    cuisineType = $('.cuisineType').val()
+    cuisineType = $('#cuisineType').val()
     intolerance = $('#intolerance').val()
     foodItem = $('#foodItem').val()
-    const cuisine = (cuisineType=='all') ? '' : `&cuisine=${cuisineType}`
-    const intolerances = (intolerance=='none') ? '' : `&intolerances=${intolerance}`
+    const cuisine = (cuisineType == 'all') ? '' : `&cuisine=${cuisineType}`
+    const intolerances = (intolerance == 'none') ? '' : `&intolerances=${intolerance}`
     const food = `&query=${foodItem}`
     let settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?offset=0&number=10" + `${cuisine}` + `${intolerances}`+ `${food}`,
+        "url": "https://webknox-recipes.p.rapidapi.com/recipes/search?offset=0&number=10" + `${cuisine}` + `${intolerances}` + `${food}`,
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
             "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
         }
     }
-    
+
     $.ajax(settings).done(function (response) {
         console.log(response)
         const results = response.results
         for (let i = 0; i < results.length; i++) {
             $('#apiCall').append(`
             <div class="card" style="width: 18rem;">
-            <h5 class="card-title">${results[i].title}</h5>
-            <img src="${response.baseUri + results[i].image}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <p class="card-text">Estimated time: ${results[i].readyInMinutes} min</p>
-            <p class="card-text"> Servings: ${results[i].servings}
-            <a href="${results[i].sourceUrl}" class="btn btn-primary">Go somewhere</a>
-            </div>
+                <h5 class="card-title">${results[i].title}</h5>
+                <img src="${response.baseUri + results[i].image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Estimated time: ${results[i].readyInMinutes} min</p>
+                    <p class="card-text"> Servings: ${results[i].servings}
+                    <a href="${results[i].sourceUrl}" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
             `)
         }
     });
 }
 
-$('#searchBtnIngredients').on("click", function(event) {
+$('#searchBtnIngredients').on("click", function (event) {
     console.log(foodItem)
 
     searchRecipeIngredients()
@@ -64,7 +64,7 @@ function searchRecipeIngredients() {
             "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
         }
     }
-    
+
     $.ajax(settings).done(function (response) {
         console.log(response);
 
@@ -82,7 +82,7 @@ function searchRecipeIngredients() {
                         "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
                     }
                 }
-                
+
                 $.ajax(settings).done(function (response) {
                     console.log(response);
                     $('#apiCall').append(`
@@ -98,8 +98,8 @@ function searchRecipeIngredients() {
                     `)
                 });
             }
-        getRecipeInfo()
+            getRecipeInfo()
         }
-        
+
     });
 }
